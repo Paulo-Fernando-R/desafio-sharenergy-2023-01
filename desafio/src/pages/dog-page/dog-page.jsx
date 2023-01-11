1031e4749971ca34ee2ee9b9c6f0716f7864cee7
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2023-01-11 15:26:14 -0300

    adicionados ajustes no css dog page

--- desafio/src/pages/dog-page/dog-page.jsx
+++ desafio/src/pages/dog-page/dog-page.jsx
@@ -1,7 +1,27 @@
 import './dog-page.css'
 import '../../global-style.css'
 import pana from '../../assets/pana.png'
+import { useState } from 'react'
 export default function DogPage(props) {
+
+    const [loading, setLoading] = useState(false)
+    const [dog, setDog] = useState(null)
+
+    const fetchData = async() => {
+        setLoading(true)
+        try{
+            const response = await fetch('https://random.dog/woof.json?filter=mp4,webm');
+            console.log(response);
+            const json = await response.json();
+            setDog(json.results)
+            console.log(json.results);
+        }
+        catch(e){
+            console.log('erro')
+        }
+        setLoading(false)
+    }
+
     return(
         <div className='dog-main-box'>
             <article>
@@ -13,7 +33,13 @@ export default function DogPage(props) {
             </section>
 
             <article>
-                <button>Carregar outro doguinho</button>
+                <button
+                    onClick={(e) => {
+                        fetchData()
+                    }}
+                >
+                    Carregar outro doguinho
+                </button>
             </article>
         </div>
     )

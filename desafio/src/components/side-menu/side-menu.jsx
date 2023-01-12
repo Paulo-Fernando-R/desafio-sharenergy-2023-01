5995d6724630b9e60ad60786e686099dfe1b67e6
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2023-01-12 20:30:21 -0300

    adicionados icones do menu

--- desafio/src/components/side-menu/side-menu.jsx
+++ desafio/src/components/side-menu/side-menu.jsx
@@ -1,7 +1,10 @@
 import './side-menu.css'
 import CodeLogo from '../../assets/code-logo.png'
 import { Link } from 'react-router-dom'
-import home from "../../assets/menu-icons/home.svg"
+import cat from "../../assets/menu-icons/cat-icon.png"
+import dog from "../../assets/menu-icons/dog-icon.png"
+import user from "../../assets/menu-icons/user-icon.png"
+import client from "../../assets/menu-icons/client-icon.png"
 import { useState } from 'react'
 
 export default function SideMenu({setSelect}){
@@ -13,10 +16,10 @@ export default function SideMenu({setSelect}){
             </div>
 
             <ul>
-                <Link onClick={() => {setSelect(0)}} className='li'><img src={home} alt="" /><li>Usuários</li></Link>
-                <Link onClick={() => {setSelect(1)}} className='li'><img src={home} alt="" /><li>Gatos</li></Link>
-                <Link onClick={() => {setSelect(2)}} className='li'><img src={home} alt="" /><li>Cachorros</li></Link>
-                <Link onClick={() => {setSelect(3)}} className='li'><img src={home} alt="" /><li>Clientes</li></Link>
+                <Link onClick={() => {setSelect(0)}} className='li'><img src={user} alt="" /><li>Usuários</li></Link>
+                <Link onClick={() => {setSelect(1)}} className='li'><img src={cat} alt="" /><li>Gatos</li></Link>
+                <Link onClick={() => {setSelect(2)}} className='li'><img src={dog} alt="" /><li>Cachorros</li></Link>
+                <Link onClick={() => {setSelect(3)}} className='li'><img src={client} alt="" /><li>Clientes</li></Link>
             </ul>
         </div>
     )
